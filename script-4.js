1f57718adfd5d35d7382bcf60816e337dfaaaf1c
PSDeSousa <paulsdesousa@hotmail.fr>
2025-03-31 15:33:43 +0200

    initial commit

--- script-4.js
+++
@@ -1,6 +0,0 @@
-let viennoiserie = ["croissants", "Pain au chocolat (on dit ici)", "Paté aux pommes (y parait on dit ici)", "brioche"];
-viennoiserie.forEach(
-    function(type){
-        alert("nous avons des délicieux " + type)
-    }
-)
--- script.js
+++ script.js
@@ -1,30 +1,75 @@
-//let lifePoint = prompt("Combien te reste t'il de points de vie ?");
-let recommencer = prompt("Continuer la partie ?");
-if (recommencer === "oui"){
-    let lifePoint = 100;
-    let experience = 0;
-} else {
-    alert("Let's go !");
+const nbCombatTotal=3;
+const maxEnergieCombat=10;
+const pointEnergieDepart=10;
+const pointEntrainement=2;
+//1. Entrainement
+//- Demander à l’utilisateur le nombre d’entrainements à faire.
+let nbEntrainement=prompt("🏋️‍♂️ Combien d'entrainements souhaitez vous faire ?");
+//- Initialiser le nombre de points d’énergie.
+let pointEnergie=pointEnergieDepart;
+//- Faire valider par l’utilisateur chaque entrainement qu’il aura fait.
+for (let i = 1; i <= nbEntrainement; i++){
+    alert("💪 Bravo c'est l'entrainement n° " + i + " du héro");
+    pointEnergie = pointEnergie + pointEntrainement;
 }
-let combat = prompt("Veux tu combattre ?");
-if (combat === "oui"){
-    let lifePoint = lifePoint + (math.round(math.random() * 10));
-    experience = experience + 10;
-    alert("Tu ne gagnera pas d'expérience !");
-} else {
-    alert("Ton expérience " + experience + "XP");
-    alert("Tes points de vie " + lifePoint + "XP");
+//- A la fin du nombre d’entrainements : afficher que l’entrainement et fini ainsi que les points d’énergie gagnés.
+alert("✅ L'entrainement est terminé");
+//1. Combats
+//- Initialiser le nombre de combats (3) à terminer.
+let nbCombat = nbCombatTotal;
+let choix = "Paschoix";
+//- Annoncer que le combat commence.
+alert("⚔️ Le combat commence !!");
+//- à chaque combat on décrémente le nombre de combats restants.
+//- à chaque combat on diminue l’énergie restante du héro.
+//- lister après chaque combat combien il en reste à faire et l’annoncer ainsi que l’énergie restante.
+while ((pointEnergie > 0) && (nbCombat > 0)){
+    choix = "Paschoix";
+    while ((choix !== "a") && (choix !== "f")){
+        choix = prompt("🔥 Un ennemie est devant toi ! Il te reste " + pointEnergie + " points d'énergie. Que fais tu ? (attaque : a / fuite : f)");
+    }
+    if (choix === "f"){
+        alert("🏃‍♂️ Le héro prend la fuite !");
+        break;
+    } else if (choix === "a"){
+        alert("⚔️ Le héro attaque un ennemie et il a " + pointEnergie + " points d'énergie");
+        nbCombat--;
+        pointEnergie = pointEnergie - Math.round((Math.random() * maxEnergieCombat));
+        if (pointEnergie>0){
+            alert("⚠️ Après l'attaque de l'Ennemie, il reste " + nbCombat + " combats à faire et le héro a encore " + pointEnergie + " points d'énergie");
+        }
+    }
 }
-if (lifePoint > 50) {
-    alert("Tu attaques avec toute ta puissance");
-} else if (lifePoint > 20 && lifePoint <= 50) {
-    alert("Tu attaques faiblement");
-} else if (lifePoint > 0 && lifePoint <= 20) {
-    alert("Tu es trop faible, tu dois fuir !");
-} else if (lifePoint <= 0) {
-    alert("Désolé, tu es décédé !");
+//- quand il n’y a plus de combat à faire, on affiche que les ennemis sont vaincus.
+if (choix === "f"){
+    alert("🛑 Fin du combat : la quète est inachevée mais le héro a survécu");
+} else if (pointEnergie <= 0){
+    alert("Le héro est mort")
 } else {
-    //Copyright Antoine !
-    alert("entre un nombre de points supérieur ou égal à 0");
+    alert("🎉  Le héro a vaincu tous ses ennemis");
+    //1. Quête des trésors 
+    //2. annoncer que le héro ouvre le coffre aux trésors
+    alert("🎁 Le héro a trouvé le coffre aux trésors")
+    //- enregistrer les différents trésors à gagner
+    let tresors = ["Épée légendaire", "Potion magique", "Bouclier en or"];
+    //- annoncer le trésor gagné à chaque trésor
+    let n=1;
+    let butin = "Le trésor est composé de : "
+    tresors.forEach(function(objet){
+        butin = butin + (" " + n + ". " + objet);
+        n++;
+    })
+    alert("✨  " + butin);
+    let rep = 0;
+    while ((rep > 3) || (rep < 1) || (isNaN(rep) === true)){
+        rep = prompt("quel trésor veux-tu prendre ? (1, 2 ou 3) ?");
+        if ((rep > 3) || (rep < 1) || (isNaN(rep) === true)) {
+            alert("Choisi un nombre entre 1 et 3 !");
+        } else {
+            alert("🎊 Tu as pris : " + tresors[rep - 1]);
+        }
+    }
+//- quand tous les trésors sont gagnés, afficher que l’aventure est terminée.
+    alert("🏆 L'aventure du héros est terminée !");
+
 }
-alert("Ton expérience " + experience + "XP");
